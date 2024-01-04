d1d3436dd80918d310de694a064211aee2dcdea0
lucasviana <lucasg.viana@outlook.com>
2024-01-04 00:15:13 -0300

    First commit: Project Structure

---
+++ CodingWiki_Model/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CodingWiki_Model.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
